b4636572f2ee20647a2961b79266dc33ff5794ec
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2025-05-31 10:00:39 +0800

    feat: update HeroLights positions and angles, adjust RectAreaLight color, and refine hero layout styles

--- src/component/HeroModels/HeroLights.jsx
+++ src/component/HeroModels/HeroLights.jsx
@@ -4,34 +4,34 @@ const HeroLights = () => {
   return (
     <>
     <spotLight
-      position={[2, 5, 6]}
-      angle={0.15}
+      position={[3, -5, -5]}
+      angle={0.50}
       intensity={100}
-      penumbra={0.2}
+      penumbra={0.5}
       color={0xffffff}
-      castShadow
+      
     />
 
     <spotLight
-      position={[4, 5, 4]}
-      angle={0.3}
-      intensity={40}
+      position={[-3, 4, 5]}
+      angle={0.50}
+      intensity={70}
       penumbra={0.5}
       color={0x4cc9f0}
-      castShadow
     />
 
     <spotLight
-      position={[-3, 5, 5]}
-      angle={0.4}
+      position={[-3, 4, -5]}
+      angle={0.5}
       intensity={60}
       penumbra={1}
       color={0x9d4edd}
-      castShadow
     />
 
     <primitive
-    object={new THREE.RectAreaLight(0xa259ff, 5, 4, 4)} 
+    object={new THREE.RectAreaLight("#e7d1ff", 2, 4, 4)} 
+    position={[0, 5, 0]}
+    penumbra={0.5}
     />
     </>
   )
