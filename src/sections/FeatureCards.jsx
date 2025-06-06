b45548f059e24ab36bdb0005f88e21a6660efbd3
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2025-06-06 15:27:36 +0800

    feat: restructure LogoSection and add FeatureCards component for enhanced layout

---
+++ src/sections/FeatureCards.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { abilities } from '../constants/index.js'
+import { div } from 'three/tsl'
+
+const FeatureCards = () => {
+  return (
+    <div className="w-full padding-x-lg">
+        <div className="mx-auto grid-3-cols">
+            {abilities.map(({ imgPath, title, desc}) => (
+                <div key={title} className='card-border rounded-xl p-8 flex flex-col gap-4'>
+                    <div className="size-14 flex items-center justify-center rounded-full">
+                        <img src={imgPath} alt={title} />
+                    </div>
+                    <h3 className='text-grey-700 text-2xl font-semibold'>{title}</h3>
+                    <p className="text-grey-900 text-xl">{desc}</p>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default FeatureCards
