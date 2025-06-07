6ffcc8349c23127c85e41f949686f2fe64091705
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2025-06-08 07:24:53 +0800

    feat: enhance GlowCards component with mouse movement effects and update ExperienceSection to pass index prop

--- src/component/GlowCards.jsx
+++ src/component/GlowCards.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
 
-const GlowCards = ({ card, children}) => {
+import React, { useRef } from 'react';
+
+const GlowCards = ({ card = {}, children, index }) => {
+  const cardRef = useRef(null);
+
+  const handleMouseMove = (e) => {
+    if (!cardRef.current) return;
+
+    // Get the mouse position relative to the card
+    const rect = cardRef.current.getBoundingClientRect();
+    const mouseX = e.clientX - rect.left - rect.width / 2;
+    const mouseY = e.clientY - rect.top - rect.height / 2;
+
+    // calculate the angle of the mouse position
+    let angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+
+    angle = (angle + 360) % 360; // Normalize angle to be between 0 and 360
+
+    cardRef.current.style.setProperty('--start', angle + 60);
+  };
+
   return (
-    <div className='card card-border timeline-card rounded-xl p-10'>
-        <div className="glow" />
-        <div className='flex items-center gap-1 mb-5'>
-            {Array.from({length: 5}, (_, i) => (
-                <img src="/images/star.png" alt="star" className='size-5' key={i} />
-            ))}
-        </div>
-        <div className='mb-5'>
-            <p className='text-grey-700 text-lg'>{card.review}</p>
-        </div>
-        {children}
+    <div
+      ref={cardRef}
+      onMouseMove={handleMouseMove}
+      className='card card-border timeline-card rounded-xl p-10'
+    >
+      <div className="glow" />
+      <div className='flex items-center gap-1 mb-5'>
+        {Array.from({ length: 5 }, (_, i) => (
+          <img src="/images/star.png" alt="star" className='star-img size-5' key={i} />
+        ))}
+      </div>
+      <div className='mb-5'>
+        <p className='text-grey-700 text-lg'>{card?.review}</p>
+      </div>
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default GlowCards
+export default GlowCards;
